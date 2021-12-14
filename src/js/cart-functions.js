a9198551797cbbe157773b51d20c2dc605816ab0
stovet <steven.topalian@gmail.com>
2021-12-14 10:30:12 -0500

    finished tests for isSufficientPayment

--- src/js/cart-functions.js
+++ src/js/cart-functions.js
@@ -2,4 +2,14 @@ function calculateChange(total, payment){
     return parseFloat((payment - total).toFixed(2));
 }
 
-module.exports = calculateChange;
+function isSufficientPayment(total, payment){
+    if(payment - total >= 0){
+        return true;
+    } return false;
+}
+
+
+module.exports = {
+    calculateChange,
+    isSufficientPayment
+}
--- tests/cart-functions.test.js
+++ tests/cart-functions.test.js
@@ -1,20 +1,32 @@
-const calculateChange = require("../src/js/cart-functions")
+const func = require("../src/js/cart-functions")
 
 describe("calculateChange", () => {
   test.todo("add calculateChange tests here");
   test("Given total 5 and payment 6, it returns 1.",function(){
-    expect(calculateChange(5, 6)).toEqual(1);
+    expect(func.calculateChange(5, 6)).toEqual(1);
   });
   test("Given total 12.30 and payment 13.03, it returns 0.73.", function(){
-    expect(calculateChange(12.30, 13.03)).toEqual(0.73);
+    expect(func.calculateChange(12.30, 13.03)).toEqual(0.73);
   });
   test("Give total 17 and payment 21", function(){
-    expect(calculateChange(17, 21)).toEqual(4);
+    expect(func.calculateChange(17, 21)).toEqual(4);
   })
 });
 
 describe("isSufficientPayment", () => {
   test.todo("add isSufficientPayment tests here");
+  test("total 5 payment 6 return true", function(){
+    expect(func.isSufficientPayment(5, 6)).toEqual(true);
+  });
+  test("total 10 payment 7 return false", function(){
+    expect(func.isSufficientPayment(10, 7)).toEqual(false);
+  });
+  test("total 3 payment 3", function(){
+    expect(func.isSufficientPayment(3, 3)).toEqual(true);
+  });
+  test("total 20 payment 10", function(){
+    expect(func.isSufficientPayment(20, 10)).toEqual(false);
+  });
 });
 
 describe("calculateTotal", () => {
